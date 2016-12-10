f2170c977d7b69a513ab155dbf66730009dddc30
Tokali's <Tokali's>
2016-12-10 12:04:00 -0600

    include signin/up forms

---
+++ js/editor/editor.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyCLWRw68W89mSHVL6akIaPNK2qxcqRaWPQ",
+        authDomain: "ilawnproject.firebaseapp.com",
+        databaseURL: "https://ilawnproject.firebaseio.com",
+        storageBucket: "ilawnproject.appspot.com",
+        messagingSenderId: "751810048512"
+      };
+      firebase.initializeApp(config);
+
+    $('#signUpButton').on('click', function() {
+        window.location = "/signup.html"
+    })
+})
---
+++ js/editor/signup.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyCLWRw68W89mSHVL6akIaPNK2qxcqRaWPQ",
+        authDomain: "ilawnproject.firebaseapp.com",
+        databaseURL: "https://ilawnproject.firebaseio.com",
+        storageBucket: "ilawnproject.appspot.com",
+        messagingSenderId: "751810048512"
+      };
+      firebase.initializeApp(config);
+})
