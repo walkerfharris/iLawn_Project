70e6f0937fddfb0916294f71c674a522ba46585a
Tokali's <Tokali's>
2016-12-14 10:52:07 -0600

    adding signup functionality, profile page and some styling

---
+++ js/editor/profile.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyCLWRw68W89mSHVL6akIaPNK2qxcqRaWPQ",
+        authDomain: "ilawnproject.firebaseapp.com",
+        databaseURL: "https://ilawnproject.firebaseio.com",
+        storageBucket: "ilawnproject.appspot.com",
+        messagingSenderId: "751810048512"
+      };
+      firebase.initializeApp(config);
+
+      var database = firebase.database();
+
+      $('#profile-header').html('Welcome ' + localStorage.getItem('userKey'));
+      $('#account-info').html(localStorage.getItem('userKey'));
+      // retrieve the username and pushID or the uuid from the local storage
+
+      var userName = localStorage.getItem('userKey')
+      var uuid = localStorage.getItem('newClientID')
+      console.log(userName);
+      console.log(uuid)
+      database.ref('/clients/' + userName + '/' + uuid).on('value', function(snapshot) {
+          // log the client data to the console
+          console.log(snapshot.val());
+          console.log(snapshot.val().userid);
+          console.log(snapshot.val().email);
+          console.log(snapshot.val().address);
+          console.log(snapshot.val().phone);
+          $('#name').html('<h3>' + snapshot.val().userid + '</h3>')
+          $('#address').html('<h4><i class="fa fa-map-marker fa-2" aria-hidden="true"></i> ' + snapshot.val().address + '</h4>')
+          $('#email').html('<h5><i class="fa fa-envelope fa-2" aria-hidden="true"></i> ' + snapshot.val().email + '</h5>')
+          $('#phone').html('<h5><i class="fa fa-phone-square fa-2" aria-hidden="true"></i> ' + snapshot.val().phone + '</h5>')
+      })
+
+});
--- js/editor/signup.js
+++ js/editor/signup.js
@@ -8,4 +8,56 @@ $(document).ready(function() {
         messagingSenderId: "751810048512"
       };
       firebase.initializeApp(config);
+
+      var database = firebase.database();
+
+      // capture the input elements
+      var emailText = $('#email');
+      var passText = $('#password');
+      var passConfText = $('#password_confirm');
+      var uidText = $('#username');
+      var addressText = $('#address');
+      var phoneText = $('#phone');
+
+      // Add sign up event
+
+      $('#signUpBtn').on('click', function() {
+
+            var email = emailText.val().trim();
+            var pass = passText.val().trim();
+            var passConf = passConfText.val().trim()
+            var uid = uidText.val().trim();
+            var address = addressText.val().trim();
+            var phone = phoneText.val().trim();
+            // signup verification if any field missing then it will show error message
+            if ((email === '') || (pass === '') || (passConf === '') || (uid === '') || (address === '') || (phone === '')) {
+              // display error message that one of the info fields is missing
+              $('#error-message').html('<p>please fill all required fields</p>')
+              return false;
+            } else if (pass !== passConf) {
+              $('#error-message').html('<p>password and password Confirm is not matching</p>')
+            } else {
+            // store the uid to localstorage
+            localStorage.clear();
+            localStorage.setItem('userKey', uid);
+            console.log(localStorage.getItem('userKey'));
+
+            // Push client info to the database
+            // the uid will be treated as the key for the client entry in the DB
+            var newClient = database.ref('/clients/' + uid).push({
+                userid: uid,
+                email: email,
+                address: address,
+                phone: phone,
+            })
+            var clientID = newClient.key;
+            localStorage.setItem('newClientID', clientID);
+            window.location.href = 'profile.html';
+            return false;
+          } // function excuted on else, ends here
+        }); // signup onClick function end here
+
+
+      console.log(localStorage.getItem('userKey'));
+      console.log(localStorage.getItem('newClientID'));
 })
